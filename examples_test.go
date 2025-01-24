b3af976dec8dfdc36d35e497965ba044df7f3dc5
Adam Duke <adam.v.duke@gmail.com>
2025-01-24 16:56:34 -0500

    add example tests for Select/Reject/Keys/Values

--- examples_test.go
+++ examples_test.go
@@ -2,6 +2,8 @@ package collect_test
 
 import (
 	"fmt"
+	"maps"
+	"slices"
 
 	"github.com/adamvduke/go-collect"
 )
@@ -50,4 +52,42 @@ func ExampleApply_structToStruct() {
 	})
 	fmt.Printf("%+v", out)
 	// Output: [{baz:aa} {baz:bba} {baz:ccca} {baz:dddda}]
+}
+
+func ExampleSelect() {
+	input := []int{1, 2, 3, 4}
+	got := collect.Select(input, func(s int) bool {
+		return s > 2
+	})
+	fmt.Println(got)
+	// Output: [3 4]
+}
+
+func ExampleReject() {
+	input := []int{1, 2, 3, 4}
+	got := collect.Reject(input, func(s int) bool {
+		return s > 2
+	})
+	fmt.Println(got)
+	// Output: [1 2]
+}
+
+func ExampleKeys() {
+	input := map[int]any{1: "1", 2: 1.2, 12: struct{}{}}
+	got := collect.Keys(input)
+	slices.Sort(got)
+	fmt.Println(got)
+	// Output: [1 2 12]
+}
+
+func ExampleValues() {
+	input := map[any]any{1: "1", 2: 1.2, 10: struct{}{}}
+	got := slices.Collect(maps.Values(input))
+	fmt.Println("contains \"1\":", slices.Contains(got, "1"))
+	fmt.Println("contains 1.2:", slices.Contains(got, 1.2))
+	fmt.Println("contains struct{}{}:", slices.Contains(got, any(struct{}{})))
+	// Output:
+	// contains "1": true
+	// contains 1.2: true
+	// contains struct{}{}: true
 }
